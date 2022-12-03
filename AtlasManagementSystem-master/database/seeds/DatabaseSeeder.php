b34d78bd33c26e0330be8a78ab5965731c4e8b45
Takahiro Mori <>
2022-12-03 09:55:06 +0900

    不要なmigration,seederファイルを削除しました。

--- AtlasManagementSystem-master/database/seeds/DatabaseSeeder.php
+++ AtlasManagementSystem-master/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(SubjectUsersTableSeeder::class);
+         $this->call(SubjectsTableSeeder::class);
     }
 }
